9fecc6d20b8f1fff78a1f613ec0506e7babecbc8
Andrew Millman <andrewmillman35@gmail.com>
2019-04-24 19:52:15 -0700

    Initial commit

---
+++ init.js
@@ -0,0 +1,9 @@
+console.log("AASSS");
+let css = chrome.extension.getURL("night.css");
+console.log(css);
+let link = document.createElement('link');
+link.type = 'text/css';
+link.rel = 'stylesheet';
+link.id = "messenger-night";
+link.href = css;
+document.head.appendChild(link);
