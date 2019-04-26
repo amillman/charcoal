60fb46c5f584b8fd104b248d4dbc21c25dc148ec
Andrew Millman <andrewmillman35@gmail.com>
2019-04-25 22:31:02 -0700

    Add toggle button in navbar

--- init.js
+++ init.js
@@ -6,29 +6,12 @@ link.id = "messenger-night";
 link.href = css;
 document.head.appendChild(link);
 
-//._4kzu on click get .uiContextualLayerBelowLeft -> ._54ng ._54nf (dropdown)
-console.log("ASS");
+let defaultLogoURL = chrome.extension.getURL("facebook-messenger.svg");
+let charcoalURL = chrome.extension.getURL("charcoal-messenger.svg");
 
 window.onload = function() {
     let settingsIcon = document.getElementsByClassName("_4kzu")[0];
-    settingsIcon.onclick = function() {
-        let settingsDropdown = document
-            .getElementsByClassName("uiContextualLayerBelowLeft")[0]
-            .getElementsByClassName("_54ng")[0]
-            .getElementsByClassName("_54nf")[0];
-
-        if (settingsDropdown.getElementsByClassName("charcoal_toggle")[0] !== undefined) {
-            return;
-        }
-
-        let settingsRow = settingsDropdown.getElementsByClassName("_54ni")[0];
-        settingsRow.insertAdjacentHTML("afterend", `
-            <li class="_54ni __MenuItem charcoal_toggle" role="presentation">
-                <a class="_54nc" href="#" role="menuitem">
-                    <span><span class="_54nh">Charcoal</span></span>
-                </a>
-            </li>
-        `);
-
-    }
+    settingsIcon.insertAdjacentHTML("afterend", `
+        <div class="charcoal_toggle" style="background-image:url('${charcoalURL}'""></div>
+    `);
 }
